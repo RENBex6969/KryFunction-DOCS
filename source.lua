c49485da1d26906e0d8e6f2d1ac13ec15a502f7e
Krypton <109706035+RENBex6969@users.noreply.github.com>
2024-08-12 21:14:39 +0800

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,6 +1,11 @@
 HttpService = game:GetService("HttpService")
 Kry = {}
 
+Kry.HttpGet = function(url)
+  data_HttpGet = game:HttpGet(url)
+  return data_HttpGet
+end
+
 Kry.getHWID = function()
   return tostring(game:GetService("RbxAnalyticsService"):GetClientId())
 end
@@ -12,6 +17,69 @@ Kry.getIP = function()
   return tostring(IP)
 end
 
+Kry.getPING = function()
+  PING = math.round(game:GetService("Stats"):FindFirstChild("PerformanceStats").Ping:GetValue())
+	return tostring(PING)
+end
+
+Kry.getMaxPING = function()
+  MaxPING = math.round(game:GetService("Stats"):FindFirstChild("PerformanceStats").MaxPing:GetValue())
+	return tostring(MaxPING)
+end
+
+
+Kry.getNetworkRECV = function()
+  NetworkRECV = game:GetService("Stats"):FindFirstChild("PerformanceStats").NetworkReceived:GetValue()
+  return tostring(NetworkRECV)
+end
+
+Kry.getMaxNetworkRECV = function()
+  MaxNetworkRECV = game:GetService("Stats"):FindFirstChild("PerformanceStats").MaxNetworkReceived:GetValue()
+  return tostring(MaxNetworkRECV)
+end
+
+Kry.getNetworkSent = function()
+  NetworkSent = game:GetService("Stats"):FindFirstChild("PerformanceStats").NetworkSent:GetValue()
+  return tostring(NetworkSent)
+end
+
+Kry.getMaxNetworkSent = function()
+  MaxNetworkSent = game:GetService("Stats"):FindFirstChild("PerformanceStats").MaxNetworkSent:GetValue()
+  return tostring(MaxNetworkSent)
+end
+
+Kry.getCPU = function()
+  CPU = math.round(game:GetService("Stats"):FindFirstChild("PerformanceStats").CPU:GetValue())
+  return tostring(CPU)
+end
+
+Kry.getMaxCPU = function()
+  MaxCPU = math.round(game:GetService("Stats"):FindFirstChild("PerformanceStats").MaxCPU:GetValue())
+	return tostring(MaxCPU)
+end
+
+
+Kry.getGPU = function()
+  GPU = math.round(game:GetService("Stats"):FindFirstChild("PerformanceStats").GPU:GetValue())
+  return tostring(GPU)
+end
+
+Kry.getMaxGPU = function()
+  MaxGPU = math.round(game:GetService("Stats"):FindFirstChild("PerformanceStats").MaxGPU:GetValue())
+	return tostring(MaxGPU)
+end
+
+
+Kry.getMEMORY = function()
+  MEM = math.round(game:GetService("Stats"):FindFirstChild("PerformanceStats").Memory:GetValue())
+  return tostring(MEM)
+end
+
+Kry.getMaxMEMORY = function()
+  MaxMEM = math.round(game:GetService("Stats"):FindFirstChild("PerformanceStats").MaxMemory:GetValue())
+	return tostring(MaxMEM)
+end
+
 Kry.getExecutorNAME = function()
   return tostring(identifyexecutor())
 end
@@ -20,6 +88,17 @@ Kry.getExecutorLVL = function()
   return tostring(getthreadidentity())
 end
 
-print("[!] Made by Ren - KryFunctions")
+Kry.spoofExecutorLVL = function(arg)
+  Kry.spoofExecutorLVL = function(arg)
+  orgprintidentity = printidentity;
+  orgsethreadidentity = setthreadidentity or set_thread_identity;
+  arg = tostring(arg)
+  KryHOOK = hookfunction(orgprintidentity, function()
+    print("Current identity is " .. arg)
+  end)
+  KryHOOK2 = hookfunction(orgsethreadidentity, function()
+    return arg
+  end)
+end
 
 return Kry
